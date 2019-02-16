720d1f69e386a484c6e13781b4f0cf4d6be5584a
Danielle Dees <d.danielle83@gmail.com>
2019-02-16 00:56:24 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+
+//setup express server and port
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+//handle data parsing
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//routing
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//listener
+
+app.listen(PORT, function() {
+    console.log("App listening on http://localhost:" + PORT);
+});
