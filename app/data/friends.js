eb19a7f74622343382fe6046f8aaabf640304868
Danielle Dees <d.danielle83@gmail.com>
2019-02-16 21:02:15 -0500

    friend data

---
+++ app/data/friends.js
@@ -0,0 +1,128 @@
+var friendArray = {
+    
+        "name":"Sterling Archer",
+        "photo":"https://i.pinimg.com/originals/6c/f2/1a/6cf21a47436087872fc3a7ef1365c0ff.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ],
+
+            "name":"Lana Kane",
+            "photo":"https://hollywoodhatesme.files.wordpress.com/2016/09/lana-kane.png?w=472",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ],
+
+              "name":"Cyril Figgis",
+            "photo":"https://cdn-images-1.medium.com/max/1200/0*TYFltiPzUa46Bq7D.jpg",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ],
+
+              "name":"Cheryl Tunt",
+            "photo":"https://uproxx.files.wordpress.com/2016/05/archer-cheryl-tunt6.jpg?quality=100&w=650&h=360",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ],
+
+              "name":"Pam Poovey",
+            "photo":"https://uproxx.files.wordpress.com/2016/05/archer-pam2.jpg?quality=100&w=650",
+            "scores":[
+                5,
+                1,
+                4,
+                4,
+                5,
+                1,
+                2,
+                5,
+                4,
+                1
+              ],
+
+              "name":"Dr. Krieger",
+              "photo":"https://uproxx.files.wordpress.com/2016/03/dr-krieger-banner.jpg?quality=100&w=650",
+              "scores":[
+                  5,
+                  1,
+                  4,
+                  4,
+                  5,
+                  1,
+                  2,
+                  5,
+                  4,
+                  1
+                ],
+
+                "name":"Ray Gillette",
+                "photo":"https://hips.hearstapps.com/esq.h-cdn.co/assets/17/15/1491924675-archer-gay-spy-ray-gillette.jpg",
+                "scores":[
+                    5,
+                    1,
+                    4,
+                    4,
+                    5,
+                    1,
+                    2,
+                    5,
+                    4,
+                    1
+                  ],
+
+                  "name":"Malory Archer",
+                  "photo":"https://www.the-arcade.ie/wp-content/uploads/2016/03/Malory-Archer.jpg",
+                  "scores":[
+                      5,
+                      1,
+                      4,
+                      4,
+                      5,
+                      1,
+                      2,
+                      5,
+                      4,
+                      1
+                    ],
+        
+      
+}
+
+  console.log(friendArray);
+
+  
